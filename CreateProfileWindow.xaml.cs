28da7b221cde9fdc2e5c9070e16665ca35b53dc2
Andrej A <atamantschuk@googlemail.com>
2023-04-06 23:31:12 +0200

    -Fehler beim Profil erstellen abgefangen (Errormessage wenn kein Name oder ein falsches Datumsformat eingegeben wurde) -Fenster werden mittig geöffnet

--- CreateProfileWindow.xaml.cs
+++ CreateProfileWindow.xaml.cs
@@ -26,9 +26,33 @@ namespace Geburtstags_Informant_WPF
 
         private void button_createProfile_Click(object sender, RoutedEventArgs e)
         {
-            ProfileManager.CreateProfile(txtBox_firstName.Text, txtBox_lastName.Text, Convert.ToDateTime(txtBox_birthDate.Text));
-            ProfileManager.AllProfiles.Add(ProfileManager.CurrentProfile);
-            Close();
+            if (string.IsNullOrEmpty(txtBox_firstName.Text) || string.IsNullOrEmpty(txtBox_lastName.Text))
+            {
+                MessageBox.Show("Bitte gebe einen Vor- und Nachnamen ein!");
+            }
+            else if (CheckBirthdayInput(txtBox_birthDate.Text) == false)
+            {
+                MessageBox.Show("Inkorrektes Datum! \n" +
+                                "Bitte gebe ein korrektes Datum ein. (TT.MM.JJJJ)");
+            }
+            else
+            {
+                ProfileManager.CreateProfile(txtBox_firstName.Text, txtBox_lastName.Text, Convert.ToDateTime(txtBox_birthDate.Text));
+                ProfileManager.AllProfiles.Add(ProfileManager.CurrentProfile);
+                Close();
+            }
+        }
+
+        private bool CheckBirthdayInput(string input)
+        {
+            if (string.IsNullOrEmpty(input) || DateTime.TryParse(input, out DateTime birthDate) == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
